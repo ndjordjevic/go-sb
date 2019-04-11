1a67f225f4342c754d656e23ba1739ea538d9ac4
ndjordjevic <ndjordjevic@gmail.com>
2019-04-11 20:40:26 +0200

    Init

---
+++ instruments-producer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	brokerList = kingpin.Flag("brokerList", "List of brokers to connect").Default("localhost:9092").Strings()
+	topic      = kingpin.Flag("topic", "Topic name").Default("instruments_topic").String()
+	maxRetry   = kingpin.Flag("maxRetry", "Retry limit").Default("5").Int()
+)
+
+func main() {
+	kingpin.Parse()
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = *maxRetry
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer(*brokerList, config)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	msg := &sarama.ProducerMessage{
+		Topic: *topic,
+		Value: sarama.StringEncoder("Something Cool4"),
+	}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", *topic, partition, offset)
+}
