27dd5a91ab7db7d5787cfda065c7daf5e8f13d22
ndjordjevic <ndjordjevic@gmail.com>
2019-04-18 18:18:50 +0200

    polish some logging

--- cmd/kafka_consumer/consumers.go
+++ cmd/kafka_consumer/consumers.go
@@ -28,6 +28,7 @@ func main() {
 	cluster := gocql.NewCluster("127.0.0.1")
 	cluster.Keyspace = "go_sb"
 	session, _ := cluster.CreateSession()
+	log.Println("Connected to Cassandra.")
 	defer session.Close()
 
 	// connect to Kafka
@@ -43,6 +44,8 @@ func main() {
 			panic(err)
 		}
 	}()
+	log.Println("Waiting on new messages...")
+
 	consumer, err := master.ConsumePartition(*topic, 0, sarama.OffsetNewest)
 	if err != nil {
 		panic(err)
@@ -58,7 +61,7 @@ func main() {
 				fmt.Println(err)
 			case msg := <-consumer.Messages():
 				*messageCountStart++
-				fmt.Println("Received messages", string(msg.Key), string(msg.Value))
+				log.Println("Received messages", string(msg.Key), string(msg.Value))
 				err := json.Unmarshal(msg.Value, &instrument)
 				if err != nil {
 					return
@@ -66,9 +69,10 @@ func main() {
 
 				date := strconv.Itoa(int(instrument.ExpirationDate.Year)) + "-" + strconv.Itoa(int(instrument.ExpirationDate.Month)) + "-" + strconv.Itoa(int(instrument.ExpirationDate.Day))
 				insertSql := "INSERT INTO instruments (market, isin, currency, short_name, long_name, expiration_date, status) VALUES ('" + instrument.Market + "', '" + instrument.ISIN + "', '" + instrument.Currency + "', '" + instrument.ShortName + "', '" + instrument.LongName + "', '" + date + "', '" + instrument.Status + "')"
-				log.Println(insertSql)
 				if err := session.Query(insertSql).Exec(); err != nil {
 					log.Fatal(err)
+				} else {
+					log.Println("Instrument is inserted/updated in Cassandra")
 				}
 			case <-signals:
 				fmt.Println("Interrupt is detected")
